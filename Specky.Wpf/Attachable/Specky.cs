5fffe103085137401de942d4f8da1630c9934c84
Michael Pucket <michael_puckett_ii@hotmail.com>
2020-12-23 12:34:33 -0500

    First attachable property for WPF using Specky.

---
+++ Specky.Wpf/Attachable/Specky.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+namespace Specky.Wpf.Attachable
+{
+    public class Ioc
+    {
+        private const string DataContext = "DataContext";
+        public static object GetDataContext(DependencyObject obj)
+            => obj.GetValue(DataContextProperty);
+        public static void SetDataContext(DependencyObject obj, object value)
+            => obj.SetValue(DataContextProperty, value);
+
+        public static readonly DependencyProperty DataContextProperty =
+            DependencyProperty.RegisterAttached(DataContext, typeof(object), typeof(Ioc), new PropertyMetadata(null,
+            new PropertyChangedCallback((s, e) =>
+            {
+                if (s is FrameworkElement frameworkElement && e.NewValue is Type type)
+                {
+                    if (frameworkElement.IsLoaded)
+                    {
+                        var speck = SpeckyContainer.Instance.GetSpeck(type);
+                        frameworkElement.DataContext = speck;
+                    }
+                    else
+                    {
+                        frameworkElement.Loaded += FrameworkElementLoaded;
+                    }
+
+                    void FrameworkElementLoaded(object sender, RoutedEventArgs e)
+                    {
+                        frameworkElement.Loaded -= FrameworkElementLoaded;
+                        var speck = SpeckyContainer.Instance.GetSpeck(type);
+                        frameworkElement.DataContext = speck;
+                    }
+                }
+            })));
+
+
+    }
+}
